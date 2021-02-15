9d71a28aea19a0241ac57489d5ba1a79490b3fdb
Eran <ecohanim@gmail.com>
2021-02-15 17:10:46 +0200

    Added swagger api documentation

---
+++ episode_7/data/products.go
@@ -0,0 +1,132 @@
+package data
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/go-playground/validator"
+)
+
+// Product defines the structure for an API product
+// swagger:model
+type Product struct {
+	// the product id
+	//
+	// required: true
+	// min: 1
+	ID          int     `json:"id"`
+	Name        string  `json:"name" validate:"required"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price" validate:"gt=0"`
+	SKU         string  `json:"sku" validate:"required,sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+// func (p *Product) FromJSON(r io.Reader) error {
+// 	d := json.NewDecoder(r)
+// 	return d.Decode(p)
+// }
+
+func (p *Product) Validate() error {
+	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
+
+	return validate.Struct(p)
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	// sku is of format abc-absd-dfsdf
+
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+	return true
+}
+
+type Products []*Product
+
+// func (p *Products) ToJSON(w io.Writer) error {
+// 	e := json.NewEncoder(w)
+// 	return e.Encode(p)
+// }
+
+// func GetProducts() []*Product {
+// 	return productList
+// }
+
+func GetProducts() Products {
+	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextId()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+// DeleteProduct deletes a product from the database
+func DeleteProduct(id int) error {
+	i, _ := findProduct(id)
+	if i == -1 {
+		return ErrProductNotFound
+	}
+
+	productList = append(productList[:i], productList[i+1])
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return i, nil
+		}
+	}
+
+	return -1, ErrProductNotFound
+}
+
+func getNextId() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Late",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
---
+++ episode_7/go.mod
@@ -0,0 +1,18 @@
+module github.com
+
+go 1.15
+
+require (
+	github.com/go-openapi/runtime v0.19.11
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
